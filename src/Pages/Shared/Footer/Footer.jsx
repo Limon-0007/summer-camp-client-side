350a4467c26794ee282a4f52f39edc4ddb49d419
Limon-0007 <limonahmed3023@gmail.com>
2023-06-07 03:38:45 +0600

    added navbar

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-base-200 text-base-content">
+  <div>
+    <Link to="/">
+        <img src="https://ld-wt73.template-help.com/tf/ispeak/images/logo-default-110x39.svg" alt="image not found" />
+    </Link>
+    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+  </div> 
+  <div>
+    <span className="footer-title">Services</span> 
+    <a className="link link-hover">Branding</a> 
+    <a className="link link-hover">Design</a> 
+    <a className="link link-hover">Marketing</a> 
+    <a className="link link-hover">Advertisement</a>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </div>
+</footer>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const user = true;
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/">Instructors</Link>
+      </li>
+      <li>
+        <Link to="/">Classes</Link>
+      </li>
+      {user && (
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+      )}
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 px-8">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold"
+          >
+            {navItems}
+          </ul>
+        </div>
+        <Link to="/">
+          <img
+            src="https://ld-wt73.template-help.com/tf/ispeak/images/logo-default-110x39.svg"
+            alt="Image not found"
+          />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-semibold">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        {!user ? (
+          <a className="btn">Button</a>
+        ) : (
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-10/girl-names-that-start-with-c-zz-221027-768b76.jpg" alt="Image not found" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
