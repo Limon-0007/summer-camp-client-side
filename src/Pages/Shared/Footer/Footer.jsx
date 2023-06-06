09f6a6d464d6ee612bd7860db93ce108dff243f0
Limon-0007 <limonahmed3023@gmail.com>
2023-06-07 03:47:11 +0600

    added navbar

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,37 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
-  <div>
-    <Link to="/">
-        <img src="https://ld-wt73.template-help.com/tf/ispeak/images/logo-default-110x39.svg" alt="image not found" />
-    </Link>
-    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
-  </div> 
-  <div>
-    <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
-  </div> 
-  <div>
-    <span className="footer-title">Company</span> 
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
-  </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
-    <a className="link link-hover">Cookie policy</a>
-  </div>
-</footer>
-    );
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content font-semibold">
+      <div>
+        <Link to="/">
+          <img
+            src="https://ld-wt73.template-help.com/tf/ispeak/images/logo-default-110x39.svg"
+            alt="image not found"
+          />
+        </Link>
+        <p>
+          <span className="text-base mt-4">
+            i Speak Industries Ltd.
+          </span>
+          <br />
+          Providing reliable tech since 1992
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">Services</span>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 px-8">
+    <div className="navbar font-semibold fixed z-10 bg-opacity-30 max-w-screen-xl bg-black md:text-white md:py-5 py-2 md:px-8 px-4">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -65,12 +65,15 @@ const Navbar = () => {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-10/girl-names-that-start-with-c-zz-221027-768b76.jpg" alt="Image not found" />
+                <img
+                  src="https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-10/girl-names-that-start-with-c-zz-221027-768b76.jpg"
+                  alt="Image not found"
+                />
               </div>
             </label>
             <ul
               tabIndex={0}
-              className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+              className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 text-black"
             >
               <li>
                 <a className="justify-between">
